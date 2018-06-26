c05785f643dd3f2fae13c0136c6c5b9a74798099
tech-camp <tech-camp@1-227.local>
2018-06-26 13:04:33 +0900

    ルートにミドルウェア追加 カチンコのルート設定

--- app/Http/Controllers/TakesController.php
+++ app/Http/Controllers/TakesController.php
@@ -5,8 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Http\Take;
 
 class TakesController extends Controller
 {
-    //
+  public function create(){
+      return view('products.takes');
+  }
+
+  public function store(Request $request){
+    Take::create(
+      array(
+        'take_number' => $request->take_number,
+        'judge' => $request->judge
+      )
+    );
+    return view('products.takes');
+  }
 }
