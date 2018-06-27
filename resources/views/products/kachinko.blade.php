b425eb98ca7a4646cc43166b18c58345dc874ab9
tech-camp <tech-camp@1-227.local>
2018-06-27 10:13:05 +0900

    カチンコ機能途中

--- resources/views/products/kachinko.blade.php
+++ resources/views/products/kachinko.blade.php
@@ -1,10 +1,28 @@
 <!DOCTYPE html>
 <html>
-  <<head>
+  <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>いつもMOVI</title>
   </head>
-  <<body>
+  <body>
+    あああああ
+    <div>
+        {{ Form::open(['url' => '/tweets', 'method' => 'post']) }}
+            <h3>
+                    作品名
+            </h3>
+            <h3>
+                    SCENE No.
+            </h3>
+            <h3>
+                    CUT No.
+            </h3>
+            <input placeholder="TAKE No." type="integer" name="take_number">
+            <input placeholder="NG=0, OK=1, PD=2" type="integer" name="judge">
+            <textarea cols="30" name="memo" placeholder="text" rows="10"></textarea>
+            <input type="submit" value="SENT">
+        {{ Form::close() }}
+    </div>
 
   </body>
 </html>
