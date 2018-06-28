795855e5f2364f4b5f87e0b0853e2e3a435b7def
tech-camp <>
2018-06-28 11:41:18 +0900

    カット一覧表示できるようになった

--- app/Http/Controllers/CutsController.php
+++ app/Http/Controllers/CutsController.php
@@ -5,8 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Cut;
 
 class CutsController extends Controller
 {
-    //
+    public function store(Request $request){
+    Cut::create(
+      array(
+        'cut_number' => $request->cut_number
+      )
+    );
+    return redirect('/users/products/scenes/cuts');
+  }
+
+  public function show(){
+    $cuts = Cut::all();
+    return view('products.cuts')->with('cuts', $cuts);
+  }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,9 +25,11 @@ Route::group(['middleware' => ['web']], function () {
   Route::post('/users/products/scenes/cuts/kachinko', 'KachinkoController@show');
   Route::get('users/products/scenes/cuts/takes', 'TakesController@show');
   Route::resource('/users', 'UsersController',['except' => 'create']);
-  Route::resource('/users/products', 'ProductsController');
-  Route::resource('/users/products/scenes','ScenesController');
-  Route::resource('/users/products/scenes/cuts','CutsController');
+  // Route::resource('/users/products', 'ProductsController');
+  // Route::resource('/users/products/scenes','ScenesController');
+  // Route::resource('/users/products/scenes/cuts','CutsController');
+  Route::post('/users/products/scenes/cuts','CutsController@store');
+  Route::get('/users/products/scenes/cuts','CutsController@show');
 
   Route::post('/users/products/scenes', 'ScenesController@store');
   Route::get('/users/products/scenes', 'ScenesController@show');
