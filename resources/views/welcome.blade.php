f1b247bf799da3dc9aa7d1108b978095679a0777
Haruno27 <haruno.k0417@gmail.com>
2018-07-11 11:51:47 +0900

    ゲスト画面UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,24 +5,41 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
     <title>いつもMOVI</title>
 
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
-
     <style>
+        html, body {
+            height: 100%;
+        }
+
         body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
             font-family: 'Lato';
+            background-color: #486d46;
+            background-image: url("/assets/images/welcome.png");
+            background-size: cover;
+            background-attachment: fixed;
+            background-position: center center;
+        }
+
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+            font-size: 38px;
+        }
+
+        .content {
+            text-align: center;
+            display: inline-block;
         }
 
         .fa-btn {
-            margin-right: 6px;
+            margin-right: 30px;
         }
     </style>
 </head>
