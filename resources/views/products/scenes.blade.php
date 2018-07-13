bdb7572d68d9911369b0eab1200127a8976f6d58
tech-camp <tech-camp@1-227.local>
2018-07-13 12:46:54 +0900

    シーン一覧UI

--- resources/views/products/scenes.blade.php
+++ resources/views/products/scenes.blade.php
@@ -9,6 +9,7 @@
     <meta charset="utf-8">
     <title>シーン一覧</title>
     <link rel="stylesheet" type="text/css" href="/css/scenes.css">
+    <link rel="stylesheet" type="text/css" href="/css/imagelist.css">
   </head>
   <body>
 
@@ -28,9 +29,51 @@
     {{ Form::close() }}
   </div>
 
-  <div>
+  <div class="contents">
+    <h2>{{$title->title}}</h2>
     @foreach($scenes as $scene)
-    <a href="/users/products/scenes/{{ $scene->id }}" title=""><li>シーンNo.{{ $scene->scene_number }}</li></a>
+    <div class="content">
+        <li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+              <div class="image">
+                @if($scene->image)
+                <img src="/assets/images/{{ $scene->image }}" alt="シーン{{ $scene->scene_number }}">
+                @else
+                <img src="/assets/images/171×180.svg" alt="{{ $scene->scene_number }}">
+                @endif
+
+              </div>
+              <div>シーン{{ $scene->scene_number }}<span class="caret"></span></div>
+            </a>
+
+              <ul class="dropdown-menu" role="menu">
+                <li>
+                  <a href="#">
+                    <i class="fa fa-btn glyphicon glyphicon-share"></i>
+                    シーン情報
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-btn glyphicon glyphicon-log-out"></i>
+                    シーン情報編集
+                  </a>
+                  <a href="/users/products/scenes/{{ $scene->id }}">
+                    <i class="fa fa-btn glyphicon glyphicon-share"></i>
+                    カット一覧
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-btn glyphicon glyphicon-log-out"></i>
+                    削除
+                  </a>
+                </li>
+              </ul>
+            </li>
+        </li>
+      </div>
     @endforeach
   </div>
 
