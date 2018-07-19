e09dd18833ff226ea048171b5a0e58f60ae8512a
Haruno27 <haruno.k0417@gmail.com>
2018-07-19 11:18:50 +0900

    カット編集画面の完成

--- resources/views/products/cuts_edit.blade.php
+++ resources/views/products/cuts_edit.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 
-{{ Form::open(['url' => "/users/products/scenes", 'method' => 'PATCH']) }}
-    <h2>カット{{$cut->id}}</h2>
-    <input placeholder="編集するカット No." type="integer" name="cut_number" value="{{ $cut->id }}">
-    <input type="submit" value="カットを編集">
+{{ Form::open(['url' => "/users/products/scenes/cuts/$cut->id/edit", 'method' => 'PATCH']) }}
+    <h2>CUT:{{$cut->cut_number}}</h2>
+    <input placeholder="編集するCUT No." type="integer" name="cut_number" value="{{$cut->cut_number}}">
+    <input type="submit" value="CUTを編集">
 
   @if (count($errors)>0)
   <div class ="error_message">
-  ※カットNo.は半角英数字で入力してください
+  ※CUT No.は半角英数字で入力してください
   </div>
   @endif
 
