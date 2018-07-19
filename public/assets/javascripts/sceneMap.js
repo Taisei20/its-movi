e00580c1feaf3af2625b0f496393ac6425033407
tech-camp <>
2018-07-19 10:18:50 +0900

    途中

--- public/assets/javascripts/sceneMap.js
+++ public/assets/javascripts/sceneMap.js
@@ -14,28 +14,26 @@ function initMap() {
     });
 
 // makerの生成
-        var markLat = locations['lat'];
-        var markLng = locations['lng'];
+  var markLat = locations['lat'];
+  var markLng = locations['lng'];
 
-        console.log(markLat);
-        console.log(markLng);
+  markers = new google.maps.Marker(
+    {
+      draggable:true,
+      position: new google.maps.LatLng(markLat, markLng),
+      map: map,
+    });
 
-        markers = new google.maps.Marker(
-        {
-          draggable:true,
-          position: new google.maps.LatLng(markLat, markLng),
-          map: map,
-        });
+// マーカーの位置情報を取得
+
 
 // windowの生成
-        infoWindow = new google.maps.InfoWindow(
-          {
-            content: '<div>' + locations['place_name'] + '</div>'
-          });
+  infoWindow = new google.maps.InfoWindow(
+    {
+      content: '<div>' + locations['place_name'] + '</div>'
+    });
 
-        markerEvent();
-
-console.log(locations);
+  markerEvent();
 
 }
 
