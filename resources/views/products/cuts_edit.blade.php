67f2511d639358c1fd523f5c13f98a3b64530ffe
Taisei20 <39693747+Taisei20@users.noreply.github.com>
2018-07-19 11:25:33 +0900

    Update cuts_edit.blade.php

--- resources/views/products/cuts_edit.blade.php
+++ resources/views/products/cuts_edit.blade.php
@@ -4,12 +4,12 @@
 
 {{ Form::open(['url' => "/users/products/scenes/cuts/$cut->id/edit", 'method' => 'PATCH']) }}
     <h2>CUT:{{$cut->cut_number}}</h2>
-    <input placeholder="編集するCUT No." type="integer" name="cut_number" value="{{$cut->cut_number}}">
-    <input type="submit" value="CUTを編集">
+    <input placeholder="編集するカット　No." type="integer" name="cut_number" value="{{$cut->cut_number}}">
+    <input type="submit" value="カットを編集">
 
   @if (count($errors)>0)
   <div class ="error_message">
-  ※CUT No.は半角英数字で入力してください
+  ※カット　No.は半角英数字で入力してください
   </div>
   @endif
 
