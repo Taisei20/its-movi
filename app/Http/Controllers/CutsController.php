4d891bc5e418c0f8e0537c8d41c565b707f59286
Daiki Rokuyama <dr19950228@gmail.com>
2018-07-20 09:48:30 +0900

    remove hoge

--- app/Http/Controllers/CutsController.php
+++ app/Http/Controllers/CutsController.php
@@ -22,71 +22,74 @@ class CutsController extends Controller
         $this->middleware('auth');
     }
 
-    public function store($id, Request $request){
-dd("hoge");
-// バリデーション
-// 半角数字以外の場合・空白の場合をエラー
-   $this->validate($request, [
+    public function store($id, Request $request)
+    {
+        // バリデーション
+        // 半角数字以外の場合・空白の場合をエラー
+        $this->validate($request, [
         'cut_number' => 'required|numeric'
-    ]);
-//
+        ]);
 
-    Cut::create(
-      array(
-        'cut_number' => $request->cut_number,
-        'scene_id' => $id
-      )
-    );
-    return redirect("/users/products/scenes/{$id}");
-  }
+        Cut::create(
+            array(
+            'cut_number' => $request->cut_number,
+            'scene_id' => $id
+            )
+        );
+        return redirect("/users/products/scenes/{$id}");
+    }
 
-  public function show($id){
-    $cuts = Cut::where('scene_id',$id)->orderBy('cut_number', 'ASC')->get();
-    $nav_scene = Scene::find($id);
-    $title = Product::find($nav_scene->product_id);
-    return view('products.cuts')->with(array('cuts' => $cuts,
+    public function show($id)
+    {
+        $cuts = Cut::where('scene_id', $id)->orderBy('cut_number', 'ASC')->get();
+        $nav_scene = Scene::find($id);
+        $title = Product::find($nav_scene->product_id);
+        return view('products.cuts')->with(array('cuts' => $cuts,
                                              'nav_scene' => $nav_scene,
                                              'title' => $title,
                                              'id' => $id
                                          ));
-  }
+    }
 
-  public function destroy($id) {
-    $scene_id = Cut::find($id)->scene_id;
-    Cut::destroy($id);
+    public function destroy($id)
+    {
+        $scene_id = Cut::find($id)->scene_id;
+        Cut::destroy($id);
 
-    return redirect("/users/products/scenes/{$scene_id}");
-  }
+        return redirect("/users/products/scenes/{$scene_id}");
+    }
 
-  public function alart($id){
-    $cut = Cut::find($id);
+    public function alart($id)
+    {
+        $cut = Cut::find($id);
 
-// ヘッダー表示用の情報取得
-    $nav_scene = Scene::find($cut->scene_id);
-    $title = Product::find($nav_scene->product_id);
-//
+    // ヘッダー表示用の情報取得
+        $nav_scene = Scene::find($cut->scene_id);
+        $title = Product::find($nav_scene->product_id);
+    //
 
-    return view('products.delete')->with(array(
-      'cut' => $cut,
-      'nav_scene' => $nav_scene,
-      'title' => $title
-    ));
-  }
+        return view('products.delete')->with(array(
+        'cut' => $cut,
+        'nav_scene' => $nav_scene,
+        'title' => $title
+        ));
+    }
 
-  public function edit($id){
-    $cut = Cut::find($id);
-    return view('products.cuts_edit')->with('cut', $cut);
-  }
+    public function edit($id)
+    {
+        $cut = Cut::find($id);
+        return view('products.cuts_edit')->with('cut', $cut);
+    }
 
-  public function update($id, Request $request){
-    $cut = Cut::find($id);
-    $cut->update(
-      array(
-        'cut_number' => $request->cut_number,
-      )
-    );
+    public function update($id, Request $request)
+    {
+        $cut = Cut::find($id);
+        $cut->update(
+            array(
+            'cut_number' => $request->cut_number,
+            )
+        );
 
-    return redirect("/users/products/scenes/{$cut->scene->id}");
-  }
-
+        return redirect("/users/products/scenes/{$cut->scene->id}");
+    }
 }
