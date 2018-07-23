93527d77c4659e8eb1ccb59eb254b13a2725622d
tech-camp <tech-camp@1-227.local>
2018-07-23 10:45:35 +0900

    ドロップダウンリスト見切れ修正

--- resources/views/products/scenes.blade.php
+++ resources/views/products/scenes.blade.php
@@ -12,72 +12,72 @@
     <link rel="stylesheet" type="text/css" href="/css/imagelist.css">
   </head>
   <body>
-
-
-  <div>
-    {{ Form::open(['url' => "/users/products/{$id}", 'method' => 'post']) }}
-
-        <input placeholder="追加するシーン No." type="integer" name="scene_number">
-        <input type="submit" value="シーンを追加">
-
-      @if (count($errors)>0)
-      <div class ="error_message">
-      ※シーンNo.は半角英数字で入力してください
-      </div>
-      @endif
-
-    {{ Form::close() }}
-  </div>
-
-  <div class="contents">
-    <h2>{{$title->title}}</h2>
-    @foreach($scenes as $scene)
     <div class="content">
-        <li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-              <div class="image">
-                @if($scene->image)
-                <img src="/assets/images/{{ $scene->image }}" alt="シーン{{ $scene->scene_number }}">
-                @else
-                <img src="/assets/images/171×180.svg" alt="{{ $scene->scene_number }}">
-                @endif
 
-              </div>
-              <div class="text">SCENE:{{ $scene->scene_number }}</div>
-            </a>
+      <div>
+        {{ Form::open(['url' => "/users/products/{$id}", 'method' => 'post']) }}
 
-              <ul class="dropdown-menu" role="menu">
-                <li>
-                  <a href="/users/products/scenes/{{ $scene->id }}/info">
-                    <i class="fa fa-btn glyphicon glyphicon-info-sign"></i>
-                    シーン情報
-                  </a>
-                </li>
-                <li>
-                  <a href="/users/products/scenes/{{ $scene->id }}/edit">
-                    <i class="fa fa-btn glyphicon glyphicon-pencil"></i>
-                    シーン情報編集
-                  </a>
-                  <a href="/users/products/scenes/{{ $scene->id }}">
-                    <i class="fa fa-btn glyphicon glyphicon-list-alt"></i>
-                    カット一覧
-                  </a>
-                </li>
-                <li>
-                  <a href="/users/products/scenes/{{ $scene->id }}/alart">
-                    <i class="fa fa-btn glyphicon glyphicon-remove"></i>
-                    削除
-                  </a>
-                </li>
-              </ul>
-            </li>
-        </li>
+          <input placeholder="追加するシーン No." type="integer" name="scene_number">
+          <input type="submit" value="シーンを追加">
+
+          @if (count($errors)>0)
+            <div class ="error_message">
+              ※シーンNo.は半角英数字で入力してください
+            </div>
+          @endif
+
+        {{ Form::close() }}
       </div>
-    @endforeach
-  </div>
 
+      <div class="contents">
+        <h2>{{$title->title}}</h2>
+        @foreach($scenes as $scene)
+          <div class="content_list">
+            <li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                  <div class="image">
+                    @if($scene->image)
+                      <img src="/assets/images/{{ $scene->image }}" alt="シーン{{ $scene->scene_number }}">
+                    @else
+                      <img src="/assets/images/171×180.svg" alt="{{ $scene->scene_number }}">
+                    @endif
 
+                  </div>
+                  <div class="text">SCENE:{{ $scene->scene_number }}</div>
+                </a>
+
+                <ul class="dropdown-menu" role="menu">
+                  <li>
+                    <a href="/users/products/scenes/{{ $scene->id }}/info">
+                      <i class="fa fa-btn glyphicon glyphicon-info-sign"></i>
+                      シーン情報
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/users/products/scenes/{{ $scene->id }}/edit">
+                      <i class="fa fa-btn glyphicon glyphicon-pencil"></i>
+                      シーン情報編集
+                    </a>
+                    <a href="/users/products/scenes/{{ $scene->id }}">
+                      <i class="fa fa-btn glyphicon glyphicon-list-alt"></i>
+                      カット一覧
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/users/products/scenes/{{ $scene->id }}/alart">
+                      <i class="fa fa-btn glyphicon glyphicon-remove"></i>
+                      削除
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </li>
+          </div>
+        @endforeach
+      </div>
+
+    </div>
   </body>
 </html>
 
--- resources/views/users/mypage.blade.php
+++ resources/views/users/mypage.blade.php
@@ -17,67 +17,67 @@
 <div class="content">
   <h1 style=" float: left">作品一覧</h1>
   <a class=" new btn btn-default" href="/users/products/create" >新規作成</a>
-</div>
 
-<div style="clear: both;"></div>
+  <div style="clear: both;"></div>
 
-<div class="row">
-  <div class="contents">
-    <ul>
-      @foreach($products as $product)
-      <div class="content">
-        <li>
-          <a href="/users/products/{{$product->id}}/flag">
-            @if($product->end_flag==0)
-              <i class="fa fa-btn glyphicon glyphicon-unchecked"></i>
-            @elseif($product->end_flag==1)
-              <i class="fa fa-btn glyphicon glyphicon-check"></i>
-            @endif
-            CRANK UP!
-          </a>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-              <div class="image">
-                @if($product->image)
-                <img src="/assets/images/{{ $product->image }}" alt="{{ $product->title }}">
-                @else
-                <img src="/assets/images/171×180.svg" alt="{{ $product->title }}">
-                @endif
-
-              </div>
-              <div class="text">{{ $product->title }}</div>
+  <div class="row">
+    <div class="contents">
+      <ul>
+        @foreach($products as $product)
+        <div class="content_list">
+          <li>
+            <a href="/users/products/{{$product->id}}/flag">
+              @if($product->end_flag==0)
+                <i class="fa fa-btn glyphicon glyphicon-unchecked"></i>
+              @elseif($product->end_flag==1)
+                <i class="fa fa-btn glyphicon glyphicon-check"></i>
+              @endif
+              CRANK UP!
             </a>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                <div class="image">
+                  @if($product->image)
+                    <img src="/assets/images/{{ $product->image }}" alt="{{ $product->title }}">
+                  @else
+                    <img src="/assets/images/171×180.svg" alt="{{ $product->title }}">
+                  @endif
+                </div>
+                <div class="text">{{ $product->title }}</div>
+              </a>
 
-            <ul class="dropdown-menu" role="menu">
-              <li>
-                <a href="/users/products/share/{{$product->id}}">
-                  <i class="fa fa-btn glyphicon glyphicon-info-sign"></i>
-                  作品情報
-                </a>
-              </li>
-              <li>
-                <a href="/users/products/{{ $product->id }}/edit">
-                  <i class="fa fa-btn glyphicon glyphicon-pencil"></i>
-                  作品情報編集
-                </a>
-                <a href="/users/products/{{$product->id}}">
-                  <i class="fa fa-btn glyphicon glyphicon-list-alt"></i>
+              <ul class="dropdown-menu" role="menu">
+                <li>
+                  <a href="/users/products/share/{{$product->id}}">
+                    <i class="fa fa-btn glyphicon glyphicon-info-sign"></i>
+                    作品情報
+                  </a>
+                </li>
+                <li>
+                  <a href="/users/products/{{ $product->id }}/edit">
+                    <i class="fa fa-btn glyphicon glyphicon-pencil"></i>
+                    作品情報編集
+                  </a>
+                  <a href="/users/products/{{$product->id}}">
+                    <i class="fa fa-btn glyphicon glyphicon-list-alt"></i>
                     シーン一覧
-                </a>
-              </li>
-              <li>
-                <a href="/users/products/{{ $product->id }}/alart">
-                  <i class="fa fa-btn glyphicon glyphicon-remove"></i>
-                  削除
-                </a>
-              </li>
-            </ul>
+                  </a>
+                </li>
+                <li>
+                  <a href="/users/products/{{ $product->id }}/alart">
+                    <i class="fa fa-btn glyphicon glyphicon-remove"></i>
+                    削除
+                  </a>
+                </li>
+              </ul>
+            </li>
           </li>
-        </li>
-      </div>
-      @endforeach
-    </ul>
+        </div>
+        @endforeach
+      </ul>
+    </div>
   </div>
+
 </div>
 
 @endsection
