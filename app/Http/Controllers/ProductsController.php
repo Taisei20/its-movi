6035ddc1c2c83dacddfee327d5c8230890cad72d
tech-camp <>
2018-07-24 12:03:37 +0900

    画像に画像ファイル以外入れるとエラー修正

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -74,7 +74,8 @@ class ProductsController extends Controller
     // バリデーション 空白の場合を入力無効
      $this->validate($request, [
                         'title' => 'required',
-                        'running_time' => 'required|numeric'
+                        'running_time' => 'required|numeric',
+                        'image' => 'image'
                      ]);
 
      if($request->image){
