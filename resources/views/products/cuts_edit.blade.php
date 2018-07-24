430cc1c0fe8ddf312d3f81838453ff3d8a9093b2
tech-camp <>
2018-07-24 10:10:15 +0900

    カットテイク編集ページ細かい言葉などの修正

--- resources/views/products/cuts_edit.blade.php
+++ resources/views/products/cuts_edit.blade.php
@@ -2,14 +2,16 @@
 
 @section('content')
 
+
+<h3>カットNo.の変更</h3>
 {{ Form::open(['url' => "/users/products/scenes/cuts/$cut->id/edit", 'method' => 'PATCH']) }}
     <h2>CUT:{{$cut->cut_number}}</h2>
-    <input placeholder="編集するカット　No." type="integer" name="cut_number" value="{{$cut->cut_number}}">
-    <input type="submit" value="カットを編集">
+    <input placeholder="変更後のカットNo." type="integer" name="cut_number" value="{{$cut->cut_number}}">
+    <input type="submit" value="カットNo.を変更">
 
   @if (count($errors)>0)
-  <div class ="error_message">
-  ※カット　No.は半角英数字で入力してください
+  <div class ="error_message" style="color: red;">
+  ※カットNo.は半角英数字で入力してください
   </div>
   @endif
 
