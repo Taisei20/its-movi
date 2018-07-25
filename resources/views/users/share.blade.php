43b0166ee183b476adec33622e9353edac79ff1b
tech-camp <>
2018-07-25 12:29:12 +0900

    copy機能の追加

--- resources/views/users/share.blade.php
+++ resources/views/users/share.blade.php
@@ -1,11 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
+
+<style>
+.copy{
+  float: right;
+}
+
+#copy{
+  width: 250px;
+}
+
+</style>
+
 <link rel="stylesheet" type="text/css" href="/css/imagelist.css">
+<script src="{{ asset('assets/javascripts/copy.js') }}" ></script>
 
 <div class="content">
 
   <div class="row">
+    <!-- copy form btn -->
+  <div class="copy">
+    <input id="copy" type="text" value="https://its-movi.herokuapp.com/users/share/{{ $name->id }}">
+    <button class="btn btn-default" onclick="copy()"><i class="fa fa-btn glyphicon glyphicon-paperclip"></i></button>
+  </div>
+
     <h1 style=" float: left">{{ $name->name }}さんの作品一覧</h1>
   </div>
 
